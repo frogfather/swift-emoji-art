5d68cfd6a4973c99d7e903d7f81c68bb8b475ade
frogfather <buyjohn@gmail.com>
2023-08-19 10:50:01 +0100

    end of lecture 11

---
+++ EmojiArt/PaletteStore.swift
@@ -0,0 +1,75 @@
+//
+//  PaletteStore.swift
+//  EmojiArt
+//
+//  Created by John Campbell on 18/08/2023.
+//
+
+import SwiftUI
+
+struct Palette: Identifiable, Codable {
+    var name: String
+    var emojis: String
+    var id: Int
+    
+    fileprivate init(name: String, emojis: String, id: Int) {
+        self.name = name
+        self.emojis = emojis
+        self.id = id
+    }
+}
+
+class PaletteStore: ObservableObject {
+    let name: String
+    
+    @Published var palettes = [Palette]() {
+        didSet {
+            storeInUserDefaults()
+        }
+    }
+    
+    private var userDefaultsKey: String {
+        "PaletteStore:" + name
+    }
+    
+    private func storeInUserDefaults() {
+        UserDefaults.standard.set(try? JSONEncoder().encode(palettes), forKey: userDefaultsKey)
+    }
+    
+    private func restoreFromUserDefaults() {
+        if let jsonData = UserDefaults.standard.data(forKey: userDefaultsKey),
+        let decodedPalettes = try? JSONDecoder().decode(Array<Palette>.self, from: jsonData) {
+            palettes = decodedPalettes
+        }
+    }
+    
+    init(named name: String) {
+        self.name = name
+        if palettes.isEmpty {
+            insertPalette(named: "Vehicles", emojis: "🚗🚕🚙🚜🚛🚲🛴🦼🏍️🛺🚡🚄🚅🚂🚇")
+        }
+    }
+    
+    // MARK: - Intent
+    
+    func palette(at index: Int) -> Palette {
+        let safeIndex = min(max(index, 0), palettes.count - 1)
+        return palettes[safeIndex]
+    }
+    
+    @discardableResult
+    func removePalette(at index: Int) -> Int {
+        if palettes.count > 1, palettes.indices.contains(index) {
+            palettes.remove(at: index)
+        }
+        return index % palettes.count
+    }
+    
+    func insertPalette(named name: String, emojis: String? = nil, at index: Int = 0) {
+        let unique = (palettes.max(by: {$0.id < $1.id})?.id ?? 0) + 1
+        let palette = Palette(name: name, emojis: emojis ?? "", id: unique)
+        let safeIndex = min(max(index,0), palettes.count)
+        palettes.insert(palette, at: safeIndex)
+    }
+    
+}
